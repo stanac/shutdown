94a94dd954ac6054c72250e6b55464d96bdb1070
Milan Stanacev <Milan Stanacev>
2016-03-05 21:05:22 +0100

    change settings and log dir

--- ShutDown/Data/DataBase.cs
+++ ShutDown/Data/DataBase.cs
@@ -5,7 +5,7 @@ namespace ShutDown.Data
 {
     public abstract class DataBase
     {
-        protected static readonly string FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ShutDown_cJ51KAf2ckeBsFCJ2wP3pQ"); // to avoid collisions
+        protected static readonly string FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ShutDown_stanac"); // to avoid collisions
 
         static DataBase()
         {
